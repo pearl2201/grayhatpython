a8f8797d1493141ff7676a52cdbcfd22ed365b2a
pearl2201 <nguyenanhngoc.ftu@gmail.com>
2020-02-18 06:53:28 +0700

    add source code

---
+++ my_debugger_defines.py
@@ -0,0 +1,47 @@
+from ctypes import *
+
+# Let's map the microsfot types to ctypes for clarity
+WORD = c_ushort
+DWORD = c_ulong
+LPBYTE = POINTER(c_ubyte)
+LPTSTR = POINTER(c_char)
+HANDLE = c_void_p
+
+
+# Constants
+DEBUG_PROCESS = 0x00000001
+CREATE_NEW_CONSOLE = 0x00000010
+# Structures for CreateProcessA() function
+# STARTUPINFO describes how to spawn the process
+class STARTUPINFO(Structure):
+    _fields_ = [
+        ("cb", DWORD),
+        ("lpReserved", LPTSTR),
+        ("lpDesktop", LPTSTR),
+        ("lpTitle", LPTSTR),
+        ("dwX", DWORD),
+        ("dwY", DWORD),
+        ("dwXSize", DWORD),
+        ("dwYSize", DWORD),
+        ("dwXCountChars", DWORD),
+        ("dwYCountChars", DWORD),
+        ("dwFillAttribute", DWORD),
+        ("dwFlags", DWORD),
+        ("wShowWindow", WORD),
+        ("cbReserved2", WORD),
+        ("lpReserved2", LPBYTE),
+        ("hStdInput", HANDLE),
+        ("hStdOutput", HANDLE),
+        ("hStdError", HANDLE),
+    ]
+
+# PROCESS_INFORMATION receives its information
+# after the target process has been successfully
+# started.
+class PROCESS_INFORMATION(Structure):
+    _fields_ = [
+        ("hProcess", HANDLE),
+        ("hThread", HANDLE),
+        ("dwProcessId", DWORD),
+        ("dwThreadId", DWORD),
+    ]
